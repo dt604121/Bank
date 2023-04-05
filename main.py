5db87cfd8e2cf3a5e87366185e4ac3c24379a93e
dt604121 <dt604@byui.edu>
2023-04-05 13:20:39 -0500

    menu choices

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello World!")
+print("Hello There! Welcome to Easy Bank :)")
+account_num = input("Account Number: ")
+pin = input("PIN: ")
+menu_choice = input("\nMenu:\nCreate Account\nLog In\nExit\n")
+if menu_choice.capitalize() == "Create Account":
+    print("Create A New Account")
+elif menu_choice.capitalize() == "Log In":
+    print("Log In")
+elif menu_choice.capitalize() == 'Exit':
+    print("Exit. Goodbye! ")
+else:
+    print("Please choose an option from the menu")
