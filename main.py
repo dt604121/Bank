11db6d7ab78fd0f6e42b59c675324256461c0ec4
dt604121 <dt604@byui.edu>
2023-04-13 12:16:36 -0500

    update to reapeating menu

--- main.py
+++ main.py
@@ -27,12 +27,14 @@ withdraw_amount = 0
 deposit_amount = 0
 deposit_difference = 0
 withdraw_difference = 0
+deposit_choice = 0
+withdraw_choice = 0
 
 def check_balance(balance):
     #use mysql info to find and pull balance and display
     print(f"Your balance is: ${balance}")
 
-def deposit(balance, deposit_amount, deposit_difference):
+def deposit(balance, deposit_amount, deposit_difference, deposit_choice):
     money_left = 0
     while deposit_choice != 1 or deposit_choice != 2:
         deposit_choice = input("1) Deposit\n2) Cancel\n")
@@ -47,7 +49,7 @@ def deposit(balance, deposit_amount, deposit_difference):
             print("Please choose either option 1 or 2.")
         display_menu(menu_choice)
 
-def withdraw(balance, withdraw_amount, withdraw_difference):
+def withdraw(balance, withdraw_amount, withdraw_difference, withdraw_choice):
     money_left = 0
     while withdraw_choice != 1 or withdraw_choice != 2:
         withdraw_choice = input("1) Withdraw\n2) Cancel\n")
@@ -79,40 +81,54 @@ def modify_account():
     # allow edit access & ability to close account, edit name, change pin number, personal identification, etc.
     print("Edit access")
 
-
-def display_menu(menu_choice, name, balance):
+def display_menu(menu_choice, name, balance, deposit_amount, deposit_difference, deposit_choice, withdraw_amount, withdraw_difference, withdraw_choice):
+    name = input("Name: ")
     while menu_choice < 1 or menu_choice > 5:
         menu_choice = int(input(
-            """\n~ Home ~\n1) Menu\n2) Create Account\n3) Delete Account\n4) Log In\n5) Exit\n\nPlease choose an option (action or the number): """))
+            """\n~ Home ~\n1) Menu\n2) Create Account\n3) Delete Account\n4) Log In\n5) Exit\n\nPlease choose an option (number 1-5): """))
         if menu_choice == 1:
-            menu_choice == 0
+            menu_choice = 0
+            display_menu(menu_choice, name, balance, deposit_amount, deposit_difference, withdraw_amount, withdraw_difference)
         elif menu_choice == 2:
             create_account(name, birth_day, pin_num)
         elif menu_choice == 3:
             delete_account()
         elif menu_choice == 4:
-            print("Log In")
             # using account number find name and display
-            print("Welcome -name-!")
+            print(f"\nWelcome {name.capitalize()}! You are now logged in :).")
             login_choice = int(input(
-                """\n~ Home ~\n1) Menu\n2) Check Balance\n3) Deposit Money\n4) Withdraw\n5) Edit Account\n6) Exit\n\nPlease choose an option (action or the number): """))
-            if login_choice == 2:
+                """\n~ Home ~\n1) Menu\n2) Check Balance\n3) Deposit Money\n4) Withdraw\n5) Edit Account\n6) Exit\n\nPlease choose an option (number 1-6): """))
+            if login_choice == 1:
+                repeat_menu(menu_choice, name, balance, deposit_amount, deposit_difference, deposit_choice, withdraw_amount, withdraw_difference, withdraw_choice)
+            elif login_choice == 2:
                 check_balance(balance)
+                repeat_menu(menu_choice, name, balance, deposit_amount, deposit_difference, deposit_choice, withdraw_amount, withdraw_difference, withdraw_choice)
             elif login_choice == 3:
-                deposit(balance)
+                deposit(balance, deposit_amount, deposit_difference, deposit_choice)
+                repeat_menu(menu_choice, name, balance, deposit_amount, deposit_difference, deposit_choice, withdraw_amount, withdraw_difference, withdraw_choice)
             elif login_choice == 4:
-                withdraw(balance)
+                withdraw(balance, withdraw_amount, withdraw_difference, withdraw_choice)
+                repeat_menu(menu_choice, name, balance, deposit_amount, deposit_difference, deposit_choice, withdraw_amount, withdraw_difference, withdraw_choice)
             elif login_choice == 5:
                 modify_account()
+                repeat_menu(menu_choice, name, balance, deposit_amount, deposit_difference, deposit_choice, withdraw_amount, withdraw_difference, withdraw_choice)
             elif login_choice == 5:
                 print("\nExit. Bye.")
+                break
             else:
                 print("Please choose a valid option of 1-6.")
         elif menu_choice == 5:
-            print("\nExit. Goodbye! ")
+            print("\nExit. Goodbye!\n")
+            break
         else:
             print("Please choose an option from the menu 1-5.")
 
+
+def repeat_menu(menu_choice, name, balance, deposit_amount, deposit_difference, deposit_choice, withdraw_amount, withdraw_difference, withdraw_choice):
+    menu_choice = 0
+    display_menu(menu_choice, name, balance, deposit_amount, deposit_difference, deposit_choice, withdraw_amount, withdraw_difference, withdraw_choice)
+
+# main
 print("""
 == == == == == == == == == == == == == == == == == == == == ==\n
             Hello There. Welcome to Easy Bank!\n
@@ -120,4 +136,4 @@ print("""
 """)
 account_num = int(input("Account Number: "))
 pin = int(input("PIN: "))
-display_menu(menu_choice, name, balance)
+display_menu(menu_choice, name, balance, deposit_amount, deposit_difference, deposit_choice, withdraw_amount, withdraw_difference, withdraw_choice)
